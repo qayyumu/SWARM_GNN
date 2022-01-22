4a247a939f0c998aed4c5c796df6c40838702c61
mrusmanqayyum <mrusmanqayyum@gmail.com>
2022-01-22 19:26:09 +0500

    Gnn util

--- GNN_sw_util.py
+++ GNN_sw_util.py
@@ -1,6 +1,10 @@
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
+import os
+import glob
+import json
+
 
 
 class MLP(keras.layers.Layer):
@@ -204,3 +208,152 @@ class OutLayer(keras.layers.Layer):
 
     def call(self, inputs):
         return self.dense(inputs) * self.bound
+
+
+def stack_time_series(time_series, seg_len, axis=2):
+    # time_series shape [num_sims, time_steps, num_agents, ndims]
+    time_steps = time_series.shape[1]
+    return np.stack([time_series[:, i:time_steps+1-seg_len+i, :, :] for i in range(seg_len)],
+                    axis=axis)
+
+
+def _load_files(file_pattern, dtype, padding=None, pad_dims=None):
+    files = sorted(glob.glob(file_pattern))
+    if not files:
+        raise FileNotFoundError(f"no files matching pattern {file_pattern} found")
+
+    all_data = []
+    for f in files:
+        data = np.load(f).astype(dtype)
+
+        if padding is not None and pad_dims is not None:
+            pad_shape = [(0, padding - s if i in pad_dims else 0) for i, s in enumerate(data.shape)]
+            data = np.pad(data, pad_shape, mode='constant', constant_values=0)
+
+        all_data.append(data)
+
+    return np.concatenate(all_data, axis=0)
+
+
+def load_data(data_path, prefix='train', size=None, padding=None, load_time=False):
+    if not os.path.exists(data_path):
+        raise ValueError(f"path '{data_path}' does not exist")
+
+    # Load timeseries data.
+    timeseries_file_pattern = os.path.join(data_path, f'{prefix}_timeseries*.npy')
+    all_data = _load_files(timeseries_file_pattern, np.float32, padding=padding, pad_dims=(2,))
+
+    # Load edge data.
+    edge_file_pattern = os.path.join(data_path, f'{prefix}_edge*.npy')
+    all_edges = _load_files(edge_file_pattern, np.int, padding, pad_dims=(1, 2))
+
+    shuffled_idx = np.random.permutation(len(all_data))
+    # Truncate data samples if `size` is given.
+    if size:
+        samples = shuffled_idx[:size]
+
+        all_data = all_data[samples]
+        all_edges = all_edges[samples]
+
+    # Load time labels only when required.
+    if load_time:
+        time_file_pattern = os.path.join(data_path, f'{prefix}_time*.npy')
+        all_times = _load_files(time_file_pattern, np.float32)
+
+        if size:
+            samples = shuffled_idx[:size]
+
+            all_times = all_times[samples]
+
+        return all_data, all_edges, all_times
+
+    return all_data, all_edges
+
+
+def preprocess_data(data, seg_len=1, pred_steps=1, edge_type=1, ground_truth=True):
+    time_series, edges = data[:2]
+    time_steps, num_nodes, ndims = time_series.shape[1:]
+
+    if (seg_len + pred_steps > time_steps):
+        if ground_truth:
+            raise ValueError('time_steps in data not long enough for seg_len and pred_steps')
+        else:
+            stop = 1
+    else:
+        stop = -pred_steps
+
+    edge_label = edge_type + 1  # Accounting for "no connection"
+
+    # time_series shape [num_sims, time_steps, num_nodes, ndims]
+    # Stack shape [num_sims, time_steps-seg_len-pred_steps+1, seg_len, num_nodes, ndims]
+    time_segs_stack = stack_time_series(time_series[:, :stop, :, :],
+                                        seg_len)
+    time_segs = time_segs_stack.reshape([-1, seg_len, num_nodes, ndims])
+    if ground_truth:
+        # Stack shape [num_sims, time_steps-seg_len-pred_steps+1, pred_steps, num_nodes, ndims]
+        expected_time_segs_stack = stack_time_series(time_series[:, seg_len:, :, :],
+                                                     pred_steps)
+        assert (time_segs_stack.shape[1] == expected_time_segs_stack.shape[1]
+                == time_steps - seg_len - pred_steps + 1)
+        expected_time_segs = expected_time_segs_stack.reshape([-1, pred_steps, num_nodes, ndims])
+    else:
+        expected_time_segs = None
+
+    edges_one_hot = utils.one_hot(edges, edge_label, np.float32)
+    edges_one_hot = np.repeat(edges_one_hot, time_segs_stack.shape[1], axis=0)
+
+    if len(data) > 2:
+        time_stamps = data[2]
+
+        time_stamps_stack = stack_time_series(time_stamps[:, :stop], seg_len)
+        time_stamps_segs = time_stamps_stack.reshape([-1, seg_len])
+
+        if ground_truth:
+            expected_time_stamps_stack = stack_time_series(
+                time_stamps[:, seg_len:], pred_steps)
+            expected_time_stamps_segs = expected_time_stamps_stack.reshape([-1, pred_steps])
+        else:
+            expected_time_stamps_segs = None
+
+        return [time_segs, edges_one_hot], expected_time_segs, [time_stamps_segs, expected_time_stamps_segs]
+
+    return [time_segs, edges_one_hot], expected_time_segs
+
+
+def off_diag_matrix(n):
+    return np.ones((n, n)) - np.eye(n)
+
+
+def one_hot(labels, num_classes, dtype=np.int):
+    identity = np.eye(num_classes, dtype=dtype)
+    one_hots = identity[labels.reshape(-1)]
+    return one_hots.reshape(labels.shape + (num_classes,))
+
+
+def load_model(model, log_dir):
+    checkpoint = os.path.join(log_dir, 'weights.h5')
+    if os.path.exists(checkpoint):
+        model.load_weights(checkpoint)
+
+
+def save_model(model, log_dir):
+    os.makedirs(log_dir, exist_ok=True)
+    checkpoint = os.path.join(log_dir, 'weights.h5')
+
+    model.save_weights(checkpoint)
+
+    return tf.keras.callbacks.ModelCheckpoint(checkpoint, save_weights_only=True)
+
+
+def load_model_params(config):
+    with open(config) as f:
+        model_params = json.load(f)
+
+    seg_len = 2 * len(model_params['cnn']['filters']) + 1
+    model_params['time_seg_len'] = seg_len
+    model_params.setdefault('edge_type', 1)
+    model_params.setdefault('output_bound')
+    model_params.setdefault('edge_aggr', {})
+
+    return model_params
+
---
+++ sim_GNN_swarm.py
@@ -0,0 +1,119 @@
+### Simulation of graph neural network for swarm simulation
+import os
+import argparse
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+from GNN_swarm import *
+from GNN_sw_util import *
+
+
+def eval_baseline(eval_data):
+    time_segs = eval_data[0]
+    return np.mean(np.square(time_segs[:, :-1, :, :] -
+                             time_segs[:, 1:, :, :]))
+
+
+def main():
+    if ARGS.train:
+        prefix = 'train'
+    elif ARGS.eval:
+        prefix = 'valid'
+    else:
+        prefix = 'test'
+
+    model_params = load_model_params(ARGS.config)
+    if ARGS.learning_rate is not None:
+        model_params['learning_rate'] = ARGS.learning_rate
+
+    # data contains edge_types if `edge=True`.
+    data = load_data(ARGS.data_dir,
+                                   prefix=prefix, size=ARGS.data_size, padding=ARGS.max_padding)
+
+    # input_data: a list which is [time_segs, edge_types] if `edge_type` > 1, else [time_segs]
+    input_data, expected_time_segs = preprocess_data(
+        data, model_params['time_seg_len'], ARGS.pred_steps, edge_type=model_params['edge_type'], ground_truth=not ARGS.test)
+    print(f"\n{prefix.capitalize()} data from {ARGS.data_dir} processed.\n")
+
+    nagents, ndims = data[0].shape[-2:]
+
+    model = SwarmNet.build_model(nagents, ndims, model_params, ARGS.pred_steps)
+    # model.summary()
+
+    load_model(model, ARGS.log_dir)
+
+    if ARGS.train:
+        checkpoint = save_model(model, ARGS.log_dir)
+
+        # Freeze some of the layers according to train mode.
+        if ARGS.train_mode == 1:
+            model.conv1d.trainable = True
+
+            model.graph_conv.edge_encoder.trainable = True
+            model.graph_conv.node_decoder.trainable = False
+
+        elif ARGS.train_mode == 2:
+            model.conv1d.trainable = False
+
+            model.graph_conv.edge_encoder.trainable = False
+            model.graph_conv.node_decoder.trainable = True
+
+        model.fit(input_data, expected_time_segs,
+                  epochs=ARGS.epochs, batch_size=ARGS.batch_size,
+                  callbacks=[checkpoint])
+
+    elif ARGS.eval:
+        result = model.evaluate(
+            input_data, expected_time_segs, batch_size=ARGS.batch_size)
+        # result = MSE
+        baseline = eval_baseline(data)
+        print('Baseline:', baseline, '\t| MSE / Baseline:', result / baseline)
+
+    elif ARGS.test:
+        prediction = model.predict(input_data)
+        np.save(os.path.join(ARGS.log_dir,
+                f'prediction_{ARGS.pred_steps}.npy'), prediction)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-dir', type=str,
+                        help='data directory')
+    parser.add_argument('--data-size', type=int, default=None,
+                        help='optional data size cap to use for training')
+    parser.add_argument('--config', type=str,
+                        help='model config file')
+    parser.add_argument('--log-dir', type=str,
+                        help='log directory')
+    parser.add_argument('--epochs', type=int, default=1,
+                        help='number of training steps')
+    parser.add_argument('--pred-steps', type=int, default=1,
+                        help='number of steps the estimator predicts for time series')
+    parser.add_argument('--batch-size', type=int, default=128,
+                        help='batch size')
+    parser.add_argument('--learning-rate', '--lr', type=float, default=None,
+                        help='learning rate')
+    parser.add_argument('--train', action='store_true', default=False,
+                        help='turn on training')
+    parser.add_argument('--train-mode', type=int, default=0,
+                        help='train mode determines which layers are frozen: '
+                             '0 - all layers are trainable; '
+                             '1 - conv1d layers and edge encoders are trainable; '
+                             '2 - edge encoders and node encoder are trainable.')
+    parser.add_argument('--max-padding', type=int, default=None,
+                        help='max pad length to the number of agents dimension')
+    parser.add_argument('--eval', action='store_true', default=False,
+                        help='turn on evaluation')
+    parser.add_argument('--test', action='store_true', default=False,
+                        help='turn on test')
+    ARGS = parser.parse_args()
+
+    ARGS.data_dir = os.path.expanduser(ARGS.data_dir)
+    ARGS.config = os.path.expanduser(ARGS.config)
+    ARGS.log_dir = os.path.expanduser(ARGS.log_dir)
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+    os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+    main()
